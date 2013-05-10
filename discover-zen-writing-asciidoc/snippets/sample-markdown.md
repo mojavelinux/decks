dd9b9a29b4148c5b2d254279b336383335a7fe29
Dan Allen <dallen@redhat.com>
2013-05-09 19:58:27 -0600

    add new AsciiDoc and Arquillian decks

---
+++ discover-zen-writing-asciidoc/snippets/sample-markdown.md
@@ -0,0 +1,18 @@
+# Document Title
+
+[AsciiDoc](http://asciidoc.org) is a lightweight markup language.
+
+This is the optional preamble (an untitled section body). Useful for
+writing simple sectionless documents consisting only of a preamble.
+
+> **Note**
+>
+> The abstract, preface, appendix, bibliography, glossary and index
+> section titles are significant (*specialsections*).
+
+## First section
+
+Document sections start at **level 1** and can nest four levels deep.
+
+* Item 1
+* Item 2
---
+++ java-enterprise-testing/snippets/GreeterTest.java
@@ -0,0 +1,19 @@
+@RunWith(Arquillian.class)
+public class GreeterTest {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "test.jar")
+            .addClass(Greeter.class)
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Inject
+    Greeter greeter;
+
+    @Test
+    public void should_create_greeting() {
+        Assert.assertEquals(
+            "Hello, Earthling!", greeter.createGreeting("Earthling"));
+    }
+}
